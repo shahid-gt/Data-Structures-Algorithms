1f93abadac0d4edf1a334195eb2e32b230757eee
shahid-gt <72201403+shahid-gt@users.noreply.github.com>
2020-10-01 17:50:32 +0530

    Update main.cpp

--- BoundedIntQueue/main.cpp
+++ BoundedIntQueue/main.cpp
@@ -8,10 +8,12 @@ int main() {
 	BoundedIntQueue q;
 	for (int i = 0; i < 50; i++) {
 		q.enqueue(i);
+		cout << q.size() << endl;
 	}
-	cout << q.size() << endl;
+	
 	for (int i = 0; i < 50; i ++) {
 		cout << q.dequeue() << endl;
+		cout<<q.size()<<endl;
 	}
-	cout << q.size() << endl;
+
 }
